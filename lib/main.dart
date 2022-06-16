bdba8d6576fdabe157a316fced5a01886c20e5f7
Dosons <dosons123@gmail.com>
2022-06-16 19:56:10 -0300

    Implement Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
+import 'package:butterfly_project/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp();
-  }
+  runApp(const MaterialApp(home: SplashScreen()));
 }
