b247f7ea82d342f7e64b29db78c2a1e6c5b06107
m-veronica-n <mvns3@aluno.ifal.edu.br>
2022-06-17 17:30:24 -0300

    Criar as rotas para todas as telas.

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,16 @@ import 'package:butterfly_project/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: SplashScreen()));
+  runApp(const MaterialApp(home: SplashScreen()
+  routs:{
+    '/': () => InicialPage(),
+      '/read_list': () => ReadList(),
+      '/reading': () => Reading(),
+      '/read': () => Read(),
+      '/paused': () => Paused(),
+      '/all': () => All(),
+      '/favorites': () => Favorites(),
+      }
+
+  ));
 }
