2e80fd1deb5fe0e45b3bfd0e8f53bc09abb13d33
Dosons <dosons123@gmail.com>
2022-07-04 12:05:43 -0300

    add pages

--- lib/pages/read_list.dart
+++ lib/pages/read_list.dart
@@ -1,3 +1,6 @@
+import 'package:butterfly_project/data/exemple_books.dart';
+import 'package:butterfly_project/widget/generic_appbar.dart';
+import 'package:butterfly_project/widget/generic_body.dart';
 import 'package:flutter/material.dart';
 
 class ReadList extends StatefulWidget {
@@ -8,9 +11,27 @@ class ReadList extends StatefulWidget {
 }
 
 class _ReadListState extends State<ReadList> {
-
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Text("Lista de leitura"),);
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(80),
+        child: GenericAppBar(
+          title: "Lista de leitura",
+          leading: IconButton(
+              icon: const Icon(Icons.arrow_back_ios_new_rounded,
+                  color: Colors.white),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.filter_alt_rounded, color: Colors.white))
+          ],
+        ),
+      ),
+      body: GenericBody(items: readList),
+    );
   }
 }
